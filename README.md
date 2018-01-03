196f5f8489cab27f5ecec6292cd3e0d130197edf
axlan <feros32@gmail.com>
2018-01-03 01:23:57 -0800

    Switched back to implementing custom find/replace operations

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 Multiline find and replace extension for Visual Studio Code
 
 # Todos
+Get basic functionality!
+
+
+# Development History
 
 There are a few aproaches one could take to add this functionality. In order of simplest to most complicated:
 
@@ -10,4 +14,6 @@ There are a few aproaches one could take to add this functionality. In order of 
 3. Option 1, but also build a GUI by openning a new editor window(s) for the find and replace options. Probably better to use a single editor, with some forced split, but that would be more complicated. See https://github.com/stef-levesque/vscode-hexdump
 4. Option 1, but also build a GUI using a html view. See https://github.com/Microsoft/vscode/issues/1833#issuecomment-175074966 , or https://github.com/zikalino/vsc-docker
 
-I'm going to start by providing an easy way to copy multiple lines as a regex and think about adding some of the other elements later.
+I initially went with aproach 2 and was able to basically get things working. I could copy multiline segments as a regex and send them to the find widget. I hit an unexpected issue in that `ctrl+f` with selected text when the regex toggle escapes special characters. Therefore I couldn't pass in \n. Since you can't detect the regex enable state and you can only toggle it, this makes this method pretty useless. I could do some crazy seqence to try to determine the regex enable state by running findNext and seeing how the selection changes, but I decided this was too hacky.
+
+I decided to go back with option 1 and finish out the basic functionality.
--- src/extension.ts
+++ src/extension.ts
@@ -8,6 +8,72 @@ const escapeStringRegexp = require('escape-string-regexp');
 
 var find_str: string = "";
 
+var replace_str: string = "";
+
+function get_selected_text(doc, selection) {
+    return doc.getText(new Range(selection.start, selection.end));
+}
+
+function get_escaped_str(txt) {
+    var escaped_str:string = escapeStringRegexp(txt);
+    escaped_str = escaped_str.replace(new RegExp("\r", 'g'), "\\r");
+    escaped_str = escaped_str.replace(new RegExp("\n", 'g'), "\\n");
+    return escaped_str;
+}
+
+function multiLineFind() {
+    if (find_str.length == 0 ) {
+        vscode.window.showInformationMessage("No Find Text Set");
+        return;
+    }
+    let selection = vscode.window.activeTextEditor.selection;
+    let active_doc = vscode.window.activeTextEditor.document;
+    let end_offset = active_doc.offsetAt(selection.end);
+    let match = active_doc.getText().indexOf(find_str, end_offset);
+    if (match >= 0) {
+        let match_start = active_doc.positionAt(match);
+        let match_end = active_doc.positionAt(match + find_str.length);
+        vscode.window.activeTextEditor.selection = new Selection(match_start, match_end);
+    } else {
+        vscode.window.showInformationMessage("End of file reached: No Matches");
+    }
+}
+
+function multiLineFindWithSelection() {
+    let selection = vscode.window.activeTextEditor.selection;
+    let active_doc = vscode.window.activeTextEditor.document;
+    let selected_txt = get_selected_text(active_doc, selection);
+    if (selected_txt.length == 0) {
+        if (find_str.length == 0) {
+            return;
+        }
+    } else {
+        find_str = selected_txt;
+    }
+    return multiLineFind();
+}
+
+
+function multiLineReplaceFindNext() {
+    if (replace_str.length == 0 ) {
+        vscode.window.showInformationMessage("No Replace Text Set");
+        return;
+    }
+    let selection = vscode.window.activeTextEditor.selection;
+    let active_doc = vscode.window.activeTextEditor.document;
+    let selected_txt = get_selected_text(active_doc, selection);
+    if (find_str == selected_txt) {
+        vscode.window.activeTextEditor.edit(editBuilder => {
+            editBuilder.replace(selection, replace_str);
+        }).then(value => {
+            if (value) {
+                multiLineFind();
+            }
+       });
+    }
+}
+
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -16,49 +82,66 @@ export function activate(context: vscode.ExtensionContext) {
     // This line of code will only be executed once when your extension is activated
     console.log('"multiline-find-and-replace" is now active!');
 
-    function runCommandOnSelection(cmd) {
-        let selection = vscode.window.activeTextEditor.selection
-        let active_doc = vscode.workspace.textDocuments[0];
-        let selected_txt = active_doc.getText(new Range(selection.start, selection.end));
-        var new_line:string = "\n"
-        if (active_doc.eol == 2) {
-            new_line = "\r\n"
-        }
-        let escaped_str = escapeStringRegexp(selected_txt).replace(new RegExp(new_line, 'g'), "\\n");
-        console.log(escaped_str);
+
+    let disposable0 = vscode.commands.registerCommand('multilinefind.multiLineFind', () => {
+        multiLineFind();
+    });
+    context.subscriptions.push(disposable0);
+
+    let disposable1 = vscode.commands.registerCommand('multilinefind.multiLineFindWithSelection', () => {
+        multiLineFindWithSelection();
+    });
+    context.subscriptions.push(disposable1);
+
+    let disposable2 = vscode.commands.registerCommand('multilinefind.copyAsRegex', () => {
+        let selection = vscode.window.activeTextEditor.selection;
+        let active_doc = vscode.window.activeTextEditor.document;
+        let selected_txt = get_selected_text(active_doc, selection);
+        let escaped_str = get_escaped_str(selected_txt)
         vscode.window.activeTextEditor.edit(editBuilder => {
             editBuilder.replace(selection, escaped_str);
         });
-        cmd(escaped_str);
-        vscode.window.activeTextEditor.show()
+        vscode.commands.executeCommand('editor.action.clipboardCopyAction');
         // undo or do another replace?
         //vscode.commands.executeCommand('undo');
         vscode.commands.executeCommand('undo');
         // vscode.window.activeTextEditor.edit(editBuilder => {
         //     editBuilder.replace(vscode.window.activeTextEditor.selection, selected_txt);
         // });
-    }
-
-    let disposable1 = vscode.commands.registerCommand('multilinefind.multiLineFind', () => {
-        // How to only toggle on?
-        // Could add text run find and check, but super hacky
-        // vscode.commands.executeCommand('toggleFindRegex');
-        runCommandOnSelection((escaped_str)=>{
-            let selection = vscode.window.activeTextEditor.selection
-            // TODO: Try find and see if the selection moves to determine if regex is on or not
-            // Need to make sure regex is disabled since findWithSelection sanatizes \n
-            vscode.commands.executeCommand('actions.findWithSelection');
-            //vscode.commands.executeCommand('editor.action.nextMatchFindAction');
-        })
-    });
-    context.subscriptions.push(disposable1);
-
-    let disposable2 = vscode.commands.registerCommand('multilinefind.copyAsRegex', () => {
-        runCommandOnSelection((escaped_str)=>{
-            vscode.commands.executeCommand('editor.action.clipboardCopyAction');
-        })
     });
     context.subscriptions.push(disposable2);
+
+    let disposable3 = vscode.commands.registerCommand('multilinefind.multiLineSetReplace', () => {
+        let selection = vscode.window.activeTextEditor.selection;
+        let active_doc = vscode.window.activeTextEditor.document;
+        let selected_txt = get_selected_text(active_doc, selection);
+        if (selected_txt.length > 0) {
+            replace_str = get_selected_text(active_doc, selection);
+        }
+    });
+    context.subscriptions.push(disposable3);
+
+    let disposable4 = vscode.commands.registerCommand('multilinefind.multiLineReplaceFindNext', () => {
+        multiLineReplaceFindNext();
+    });
+    context.subscriptions.push(disposable4);
+
+    let disposable5 = vscode.commands.registerCommand('multilinefind.multiLineReplaceAll', () => {
+        if (replace_str.length == 0 ) {
+            vscode.window.showInformationMessage("No Replace Text Set");
+            return;
+        }
+        if (find_str.length == 0 ) {
+            vscode.window.showInformationMessage("No Find Text Set");
+            return;
+        }
+        let active_doc = vscode.window.activeTextEditor.document;
+        let replaced_txt = active_doc.getText().replace(new RegExp(find_str, 'g'), replace_str);
+        vscode.window.activeTextEditor.edit(editBuilder => {
+            editBuilder.replace(new Range(0, 0, active_doc.lineCount, 0), replaced_txt);
+        });
+    });
+    context.subscriptions.push(disposable5);
 }
 
 // this method is called when your extension is deactivated
